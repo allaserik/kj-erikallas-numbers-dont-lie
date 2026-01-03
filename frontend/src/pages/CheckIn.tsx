16c53a25d8fddd038d2d6032373bec3b22469722
Erik Allas <allaserik@gmail.com>
2026-01-03 18:49:09 +0200

    frontend: fix tailwind

--- frontend/src/pages/CheckIn.tsx
+++ frontend/src/pages/CheckIn.tsx
@@ -1,3 +1,8 @@
 export default function CheckIn() {
-  return <div className="p-4">Check-in</div>;
+  return (
+  <>
+    <div className="p-4">Check-in</div>
+    <div className="p-4 text-lg font-semibold italic">Numbers Don’t Lie</div>
+  </>  
+  );
 }
